abf3ae92bb9f1c55b09df573023263e77c40de71
Johnson Liu <jyliu17@gmail.com>
2020-09-01 02:16:36 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
      if array[index].class == String
+       element.is_a?(String)
 end
   
 end
