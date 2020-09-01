9ebe3239706abcef1c51d9e86c62aab7101c48ea
Johnson Liu <jyliu17@gmail.com>
2020-09-01 02:19:08 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+     
+     array.count do |strings|
      select {|v| v.class == String}
 end
   
